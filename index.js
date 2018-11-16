eca43f23262bccaaec98b412bf2c7ef965e3660b
Upasna Langar <langarupasna.2@gmail.com>
2018-11-16 05:36:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function shout(string) 
 {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) 
 {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string)
 {
-   console.log(string.toUpperCase()) 
+   console.log(string.toUpperCase());
 }
+
